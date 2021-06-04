e43f92247ca649323ab7127c75520d784e5317ec
dawn <belmont95@live.com>
2021-06-04 14:27:42 -0600

    Moderating backoff a bit on successes and adjusting loop to actually tell you if odd edge-case error codes crop up

--- twitter_fetch.py
+++ twitter_fetch.py
@@ -14,6 +14,11 @@ headers = {"Authorization": f"Bearer {bearer_token}"}
 
 backoff_multiplier = 0
 
+# Status codes:
+# 200: Okay
+# 429: Too many requests
+# 503: Service unavailable
+
 def endpoint_call(params) -> dict:
 	"""
 	Makes a call to the previously-specified endpoint with the previously specified credentials
@@ -21,44 +26,21 @@ def endpoint_call(params) -> dict:
 	To retrieve many tweets, use fetch_tweets.
 	"""
 	global backoff_multiplier
-	while backoff_multiplier == 0 or response.status_code == 429:
+	while backoff_multiplier == 0 or response.status_code != 200:
 		time.sleep(int(1.5**backoff_multiplier))
 		print("backoff_multiplier", backoff_multiplier)
 		response = requests.request("GET", search_url, headers=headers, params=params)
 		print(response.status_code)
-		if response.status_code == 429:
-			backoff_multiplier += 1
-		if response.status_code == 503:
-			time.sleep(3600)
-		if response.status_code != 429 and response.status_code != 200 and response.status_code!=503 or backoff_multiplier > 21:
-			raise Exception(response.status_code, response.text)
-		if response.status_code == 200:
+		if response.status_code == 429:     # Too many requests
+			backoff_multiplier += 1     # Increase backoff
+		elif response.status_code == 503:   # Service unavailable
+			time.sleep(3600)            # Sleep for 6 minutes
+		elif response.status_code == 200:   # Success
+			if backoff_multiplier > 0:
+				backoff_multiplier -= 1
 			return response.json()
-
-def format_params(start_time: str, end_time:str, longlatrad:str = None, lang_code:str='en',
-                  include_rts:bool = False, include_qts:bool = False,
-                  include_replies:bool = False, include_ads:bool = False, user=None) -> Dict[str, str]:
-	"""
-	Formats the provided parameters for calls to endpoint_call or fetch_tweets.
-	"""
-	include_rts_query = "" if include_rts else "-is:retweet"
-	include_qts_query = "" if include_qts else "-is:quote"
-	include_ads_query = "" if include_ads else "-is:nullcast"
-	include_replies_query = "" if include_replies else "-is:reply"
-	point_radius_query = "" if longlatrad is None else f"point_radius:{longlatrad}"
-	user_query = '' if user is None else f'from:{user}'
-	lang_query = '' if lang_code is None else f'lang:{lang_code}'
-
-	# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
-	# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-	query_params = {'query': f'{user_query} {lang_query} {point_radius_query} {include_rts_query} {include_qts_query} {include_ads_query} {include_replies_query}',
-	                'tweet.fields': 'id,author_id,geo,created_at,text,lang',
-	                'start_time': start_time,
-	                'end_time': end_time}
-
-	query_params['max_results']=500
-
-	return query_params
+		elif backoff_multiplier > 21:
+			raise Exception("Status code", response.status_code, "Text", response.text, "Multiplier", backoff_multiplier)
 
 def fetch_tweets(params:Dict[str, str]) -> Generator[dict, None, None]:
 	"""
@@ -78,6 +60,34 @@ def fetch_tweets(params:Dict[str, str]) -> Generator[dict, None, None]:
 		for tweet_returned in tweets_returned:
 			yield tweet_returned
 		time.sleep(1)
+
+
+def format_params(start_time: str, end_time:str, longlatrad:str = None, lang_code:str='en',
+                  include_rts:bool = False, include_qts:bool = False,
+                  include_replies:bool = False, include_ads:bool = False, user=None) -> Dict[str, str]:
+	"""
+	Formats the provided parameters for calls to endpoint_call or fetch_tweets.
+	"""
+	include_rts_query = "" if include_rts else "-is:retweet"
+	include_qts_query = "" if include_qts else "-is:quote"
+	include_ads_query = "" if include_ads else "-is:nullcast"
+	include_replies_query = "" if include_replies else "-is:reply"
+	point_radius_query = "" if longlatrad is None else f"point_radius:{longlatrad}"
+	user_query = '' if user is None else f'from:{user}'
+	lang_query = '' if lang_code is None else f'lang:{lang_code}'
+
+	# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+	# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+	query_params = {'query': f'{user_query} {lang_query} {point_radius_query} {include_rts_query} {include_qts_query} {include_ads_query} {include_replies_query}',
+	                'tweet.fields': 'id,author_id,geo,created_at,text,lang,conversation_id,public_metrics,source',
+	                'start_time': start_time,
+	                'end_time': end_time}
+
+	query_params['max_results']=500
+
+	return query_params
+
+
 
 if __name__ == "__main__":
 	params=format_params(user="DEM1995",
