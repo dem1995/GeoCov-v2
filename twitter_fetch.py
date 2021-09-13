ea1ea6197973d58b06347d9c5112be9c024108b9
dawn <belmont95@live.com>
2021-09-13 04:49:23 -0600

    Fixing issue with response not being set on subsequent calls toendpoint_call

--- twitter_fetch.py
+++ twitter_fetch.py
@@ -26,7 +26,9 @@ def endpoint_call(params) -> dict:
 	To retrieve many tweets, use fetch_tweets.
 	"""
 	global backoff_multiplier
-	while backoff_multiplier == 0 or response.status_code != 200:
+	endpoint_call_initial_run = True
+	while backoff_multiplier == 0 or endpoint_call_initial_run or response.status_code != 200:
+		endpoint_call_initial_run = False
 		time.sleep(int(1.5**backoff_multiplier))
 		print("backoff_multiplier", backoff_multiplier)
 		response = requests.request("GET", search_url, headers=headers, params=params)
@@ -62,7 +64,7 @@ def fetch_tweets(params:Dict[str, str]) -> Generator[dict, None, None]:
 		time.sleep(1)
 
 
-def format_params(start_time: str, end_time:str, longlatrad:str = None, lang_code:str='en',
+def format_params(start_time: str, end_time:str, longlatrad:str = None, place_id:int = None, lang_code:str='en',
                   include_rts:bool = False, include_qts:bool = False,
                   include_replies:bool = False, include_ads:bool = False, user=None) -> Dict[str, str]:
 	"""
@@ -73,12 +75,13 @@ def format_params(start_time: str, end_time:str, longlatrad:str = None, lang_cod
 	include_ads_query = "" if include_ads else "-is:nullcast"
 	include_replies_query = "" if include_replies else "-is:reply"
 	point_radius_query = "" if longlatrad is None else f"point_radius:{longlatrad}"
+	place_id_query = "" if place_id is None else f"place:{place_id}"
 	user_query = '' if user is None else f'from:{user}'
 	lang_query = '' if lang_code is None else f'lang:{lang_code}'
 
 	# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 	# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-	query_params = {'query': f'{user_query} {lang_query} {point_radius_query} {include_rts_query} {include_qts_query} {include_ads_query} {include_replies_query}',
+	query_params = {'query': f'{user_query} {lang_query} {point_radius_query} {place_id_query} {include_rts_query} {include_qts_query} {include_ads_query} {include_replies_query}',
 	                'tweet.fields': 'id,author_id,geo,created_at,text,lang,conversation_id,public_metrics,source',
 	                'start_time': start_time,
 	                'end_time': end_time}
